678f480d6e62cb72be23d8e33a407c1fc41c6f4a
shrush07 <shrupatel07@gmail.com>
2025-04-28 14:08:23 -0400

    environment.ts updated

--- src/enviornments/environment.prod.ts
+++ src/enviornments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-    production: true
-  };
+  production: true,
+  apiUrl: process.env['API_URL'], 
+  stripePublicKey: process.env['STRIPE_PUBLIC_KEY'],
+  stripeSecretKey: process.env['STRIPE_SECRET_KEY']
+};
--- src/enviornments/environment.ts
+++ src/enviornments/environment.ts
@@ -1,7 +1,9 @@
-// In environment.ts for the frontend
 export const environment = {
-  production: false,
-  apiUrl: process.env.API_URL, 
-  stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
-};
+    production: false,
+    apiUrl: 'http://localhost:5000/api',
+    stripePublicKey:'pk_test_51Qc8fmJu0UngGxbYyWkfNKhw8I3tZCMLysCPjvRvPXHOlyGQLtXMqgNO0H0erjhpqpvYaXXJfnbQnqVWPSYqM5RK00iOyRlXD4',
+    stripeSecretKey:'sk_test_51Qc8fmJu0UngGxbYkxKGYbPEXIvlAvGgBSN4Tqz9sE2oymYJIOxMw7sgUoSOXe32pNsD0LdA6vmif8jiK5h7aAN300vGAXfgL2'
+
+  };
+  
+  
