63911b748922158ee6fbcee3a020cf2a46a49354
shrush07 <shrupatel07@gmail.com>
2025-04-28 14:37:21 -0400

    environment.ts updated

--- src/enviornments/environment.prod.ts
+++ src/enviornments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
-  production: true, 
+  production: true,
   apiUrl: 'http://localhost:5000/api',
-  stripePublicKey: process.env['NG_APP_STRIPE_PUBLIC_KEY'] || ''
+  stripePublicKey: '51Qc8fmJu0UngGxbYyWkfNKhw8I3tZCMLysCPjvRvPXHOlyGQLtXMqgNO0H0erjhpqpvYaXXJfnbQnqVWPSYqM5RK00iOyRlXD4', 
 };
+
+
+// export const environment = {
+//   production: true, 
+//   apiUrl: 'http://localhost:5000/api',
+//   stripePublicKey: process.env['NG_APP_STRIPE_PUBLIC_KEY'] || ''
+// };
--- src/enviornments/environment.ts
+++ src/enviornments/environment.ts
@@ -1,6 +1,12 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/api', 
-  stripePublicKey: process.env['NG_APP_STRIPE_PUBLIC_KEY'] || ''
-};
+    production: false,
+    apiUrl: 'http://localhost:5000/api',
+    stripePublicKey:'pk_test_51Qc8fmJu0UngGxbYyWkfNKhw8I3tZCMLysCPjvRvPXHOlyGQLtXMqgNO0H0erjhpqpvYaXXJfnbQnqVWPSYqM5RK00iOyRlXD4'
+  };
+  
+  // export const environment = {
+  //   production: false,
+  //   apiUrl: 'http://localhost:5000/api', 
+  //   stripePublicKey: process.env['NG_APP_STRIPE_PUBLIC_KEY'] || ''
+  // };
   
