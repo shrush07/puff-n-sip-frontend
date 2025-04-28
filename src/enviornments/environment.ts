463cfe7b45aebf9d445f46637b7bd1e796008a84
shrush07 <shrupatel07@gmail.com>
2025-04-28 14:15:52 -0400

    environment.ts updated

--- src/enviornments/environment.ts
+++ src/enviornments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:5000/api',
-    stripePublicKey:'pk_test_51Qc8fmJu0UngGxbYyWkfNKhw8I3tZCMLysCPjvRvPXHOlyGQLtXMqgNO0H0erjhpqpvYaXXJfnbQnqVWPSYqM5RK00iOyRlXD4',
-    stripeSecretKey:'sk_test_51Qc8fmJu0UngGxbYkxKGYbPEXIvlAvGgBSN4Tqz9sE2oymYJIOxMw7sgUoSOXe32pNsD0LdA6vmif8jiK5h7aAN300vGAXfgL2'
+    stripePublicKey:'pk_test_51Qc8fmJu0UngGxbYyWkfNKhw8I3tZCMLysCPjvRvPXHOlyGQLtXMqgNO0H0erjhpqpvYaXXJfnbQnqVWPSYqM5RK00iOyRlXD4'
 
   };
   
