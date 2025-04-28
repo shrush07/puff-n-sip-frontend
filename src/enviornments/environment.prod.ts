1ca1f18693ef73558a01d267ef600efe3aa02095
shrush07 <shrupatel07@gmail.com>
2025-04-28 14:27:07 -0400

    environment.ts updated

--- src/enviornments/environment.prod.ts
+++ src/enviornments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: process.env['API_URL'], 
-  stripePublicKey: process.env['STRIPE_PUBLIC_KEY'],
-  stripeSecretKey: process.env['STRIPE_SECRET_KEY']
+  production: true, 
+  apiUrl: 'http://localhost:5000/api',
+  stripePublicKey: process.env['NG_APP_STRIPE_PUBLIC_KEY'] || ''
 };
--- src/enviornments/environment.ts
+++ src/enviornments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000/api'
-  };
-  
-  
+  production: false,
+  apiUrl: 'http://localhost:5000/api', 
+  stripePublicKey: process.env['NG_APP_STRIPE_PUBLIC_KEY'] || ''
+};
