b97c4cd223bffc64b403e523dead7f5e75dfbead
shrush07 <shrupatel07@gmail.com>
2025-04-29 07:47:38 -0400

    data.ts updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://puff-sip.onrender.com',
   stripePublicKey:'pk_test_51Qc8fmJu0UngGxbYyWkfNKhw8I3tZCMLysCPjvRvPXHOlyGQLtXMqgNO0H0erjhpqpvYaXXJfnbQnqVWPSYqM5RK00iOyRlXD4',
   stripeSecretKey:'sk_test_51Qc8fmJu0UngGxbYkxKGYbPEXIvlAvGgBSN4Tqz9sE2oymYJIOxMw7sgUoSOXe32pNsD0LdA6vmif8jiK5h7aAN300vGAXfgL2'
 };
